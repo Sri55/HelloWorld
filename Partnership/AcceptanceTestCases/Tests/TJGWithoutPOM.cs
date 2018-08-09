d2d2df493ca0f9d09c93323b3c7bc316e8c26ea7
Sri55 <sris.testing@gmail.com>
2018-08-09 15:44:01 +0100

    Add files via upload

---
+++ Partnership/AcceptanceTestCases/Tests/TJGWithoutPOM.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OpenQA.Selenium.Chrome;
+using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Support.UI;
+using System;
+using System.Collections.Generic;
+using System.Threading;
+using OpenQA.Selenium.Firefox;
+using OpenQA.Selenium.IE;
+using OpenQA.Selenium.Remote;
+
+namespace AcceptanceTestCases.Tests
+{
+    [TestClass]
+    public class TJGWithoutPOM
+    {
+        private IWebDriver driver;
+        private readonly By _jobtypeLocator = By.Id("JobType");
+
+        public  TJGWithoutPOM()
+        {
+           
+        }
+
+        [TestInitialize]
+        public void Setup()
+        {
+                      
+            driver = new InternetExplorerDriver();
+            driver.Navigate().GoToUrl("https://www.cwjobs.co.uk/");
+       }
+
+        [TestMethod]
+        public void CwJobs()
+        {
+
+          var x=  driver.FindElement(By.Id("more-options-toggle"));
+            x.Click();//Click on More Options button
+
+            var dropdown = driver.FindElement(By.XPath("//select[@id='JobType']"));
+            SelectElement selectjobtype = new SelectElement(dropdown);//Select Job type dropdown
+            selectjobtype.SelectByValue("10");
+            IList<IWebElement> options = selectjobtype.Options;
+            IWebElement firstOption = options[1];
+            NUnit.Framework.Assert.AreEqual(firstOption.Text, "Permanent");
+            Console.WriteLine("JobType contains: " + firstOption.Text);
+
+            //Taking a full-screen screenshot
+            Screenshot screenshot = ((ITakesScreenshot)driver).GetScreenshot();
+            screenshot.SaveAsFile(@"C:\Users\srilatha.devireddy\Desktop\i.jpeg", ScreenshotImageFormat.Jpeg);
+
+        }
+
+        [TestMethod]
+        public void CompaniesListed()
+        {
+            IWebElement element = driver.FindElement(By.XPath("//div[@class='counter']/h2/span[2]"));
+            Console.WriteLine("Companies listed total : " + element.Text);
+
+        }
+
+         
+
+
+      
+        [TestCleanup]
+        public void Teardown()
+        {
+            driver.Close();
+
+        }
+
+
+    }
+}
---
+++ Partnership/AcceptanceTestCases/Tests/VerifyAcceptanceTests.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Features;
+using NUnit.Framework;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium;
+using OpenQA.Selenium.IE;
+using OpenQA.Selenium.Support.PageObjects;
+
+namespace AcceptanceTestCases
+{
+    [TestClass]
+    public class VerifyAcceptanceTests
+    {
+        public IWebDriver driver;
+      
+
+        [SetUp]
+        public void SetUp()
+        {
+            driver = new InternetExplorerDriver();
+        }
+
+        [TestMethod]
+       public void One()
+        {
+            FreeCycle _cycles = new FreeCycle(driver);
+        
+            _cycles.GotoWebsite();
+            SearchResults _search = _cycles.SearchForGroup();
+            _search.VerifySearchResultsAreAsExpected();
+
+
+        }
+
+        [TearDown]
+        public void Teardown()
+        {
+            driver.Close();
+        }
+    }
+}
+
